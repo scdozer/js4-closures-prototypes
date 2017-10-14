246582e7c9af65dc0069a376d28d2ad41746739f
Scott Cardoza <Scott@Scotts-MacBook-Pro.local>
2017-10-14 10:50:37 -0600

    Comleted closures and constructors

--- closures.js
+++ closures.js
@@ -8,188 +8,203 @@ function outer() {
       return 'The original name was ' + name;
     }
   }
-  
+
   /****** INSTRUCTIONS PROBLEM 1 ******/
   /* Above you're given a function that returns another function which has a
   closure over the name variable. Invoke outer saving the return value into
   another variable called 'inner'. */
-  
+
   // Code Here
-  
-  //Once you do that, invoke inner.
-  
+
+  var inner = outer();
+  inner();
+
   //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-02
   \******************************************************************************/
-  
-  
+
+
   function callFriend(name) {
     function dial(number) {
       return 'Calling ' + name + ' at ' + number
     }
     return dial;
   }
-  
+
   /****** INSTRUCTIONS PROBLEM 2 ******/
   /* Above you're given a callFriend function that returns the dial function.
-  
+
   Store the result of invoking callFriend in a variable named callJake.
-  
-  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
+
+  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248'
   (HINT: You will need to pass in arguments to both function invocations)
   */
-  
-    //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+  var callJake = callFriend('Jake');
+  callJake('435-555-9248');
+
+// callFriend('Jake')('435-555-9248'); is the same
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-03
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 3 ******/
   /* Write a function called makeCounter that makes the following code work
   properly. */
-  
-  //Code Here
-  
+
+    function makeCounter() {
+      var num = 1;
+      return function() {
+        return num ++;
+      }
+    }
+
+    // or
+
+    function makeCounter2() {
+      var num = 0;
+      return function() {
+        return ++num;
+      }
+    }
+
   //Uncomment this once you make your function
-  //   var count = makeCounter();
-  //   count(); // 1
-  //   count(); // 2
-  //   count(); // 3
-  //   count(); // 4
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    var count = makeCounter();
+    count(); // 1
+    count(); // 2
+    count(); // 3
+    count(); // 4
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-04
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 4 ******/
   /* Inside the function called counterFactory return two functions that implement
   up/down counter. The first function is called inc, this function is responsible
-  for incrementing the value once and returning the updated value. The second function 
-  is called dec, this function is responsible for decrementing the value by one 
-  and returning the updated value. You will need to use the module pattern to 
-  achieve this. Information on the module pattern available here: 
+  for incrementing the value once and returning the updated value. The second function
+  is called dec, this function is responsible for decrementing the value by one
+  and returning the updated value. You will need to use the module pattern to
+  achieve this. Information on the module pattern available here:
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
   */
-  
-  function counterFactory(value) {
-  
-    // Code here.
-  
-  
-    return {
 
-    }
+function counterFactory(value){
+  return{
+    inc: () => ++value,
+    dec: () => --value
   }
-  
-  
+}
+
   counter = counterFactory(10);
-  // counter.inc() // 11
-  // counter.inc() // 12
-  // counter.inc() // 13
-  // counter.dec() // 12
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  counter.inc() // 11
+  counter.inc() // 12
+  counter.inc() // 13
+  counter.dec() // 12
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-05
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 5 ******/
   /* Inside the motivation function create another function called message that
   will return the welcome text with the firstname and lastname. The final message should
-  say "You're doing awesome, keep it up firstname lastname." 
+  say "You're doing awesome, keep it up firstname lastname."
   (Hint: don't forget to have a space between the firstname and lastname and a period
   at the end of the sentence.)  */
-  
+
   function motivation(firstname, lastname) {
-  
+
     var welcomeText = 'You\'re doing awesome, keep it up ';
-  
-    // code message function here.
-  
-  
+
+    function message(){
+      return welcomeText + firstname + ' ' + lastname + '.';
+    }
     //Uncommment this to return the value of your message function
-    //return message;
-  
+    return message;
+
   }
-  
+
   var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-06
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 6 ******/
   /* Inside the module's return object create a publicMethod function that
   invokes privateMethod (return the result). Invoke this by calling module.publicMethod(); outside
   the module scope */
-  
+
   var module = (function() {
     var person = {
       name: "phillip",
       age: 29,
       location: "Utah"
     };
-  
+
     function privateMethod(){
       return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
     }
-  
+
     // Anything that is being returned is made public and can be invoked from
     // outside our lexical scope
     return {
-      // Code here.
+     publicMethod:  () => {
+       return privateMethod();
+     }
     };
-  
+
   })();
-  
-  
-  
+
+
+
+
+
   /******************************************************************************\
    #PROBLEM-07
    \******************************************************************************/
@@ -202,16 +217,21 @@ function outer() {
     var secret = 143;
 
     return {
-      // Code here
+      addToSecret: (num) =>{
+        return secret = secret + num;
+      },
+      takeAwayFromSecret:(num) =>{
+        return secret = secret - num;
+      }
     }
   }
-  
-  
-  
+
+
+
   /******************************************************************************\
       #PROBLEM-08
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 8 ******/
   /* Here we have a for loop that will iterate as long as i is less than or equal
   to 5. What we need to do is console.log(i) so that it logs like so:
@@ -224,16 +244,35 @@ function outer() {
    However, because each call to console.log occurs after the loop has finished,
    the value of i has changed before the console.log executes. We'll need to use
    a closure to preserve a reference to i at the time of execution.
-  
+
    Fix the code below to log the desired output.
    */
-  
+
+  // function timeOutCounter() {
+  //     for (var i = 0; i <= 5; i++) {
+  //       setTimeout(function() {
+  //           console.log(i)
+  //       }, i * 1000)
+  //     }
+  // }
+
+  // function timeoutCounter() {
+  //   for (var i = 0; i <= 5; i++) {
+  //     (function (j) {
+  //       return setTimeout(function(){
+  //         console.log(j);
+  //       }, j*1000)
+  //     })(i);
+  //   }
+  // }
+
   function timeOutCounter() {
+    var counter = 0;
     for (var i = 0; i <= 5; i++) {
-      setTimeout(function() {
-          console.log(i)
-      }, i * 1000)
+      setTimeout( function() {
+        console.log(counter++);
+      }, i * 1000);
     }
   }
+
   timeOutCounter();
-  
--- constructors.js
+++ constructors.js
@@ -2,12 +2,16 @@
 // Problem 1
 // ======================
 
-// Create a constructor function called CarFactory that will build car objects with make and model properties. 
+// Create a constructor function called CarFactory that will build car objects with make and model properties.
 
-// The values for the make and model will come from two parameters, make and model (in that order). 
+// The values for the make and model will come from two parameters, make and model (in that order).
 
 // Code here
 
+function CarFactory (make, model){
+  this.make = make;
+  this.model = model;
+}
 
 
 
@@ -25,9 +29,7 @@ function Employee(name, email, hireDate) {
     this.hireDate = hireDate;
 }
 
-// Code here
-
-
+var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98');
 
 
 
@@ -41,17 +43,25 @@ function Employee(name, email, hireDate) {
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
 // Code here
-
-
-
+function Car(make, type, year) {
+    this.make = make;
+    this.type = type;
+    this.year = year;
+    this.move = 0;
+    return {
+      moveCar: () => {
+        return this.move += 10;
+      }
+    }
+    }
 
   //Function Invocations Here
 
   var prius = new Car('Toyota', 'Prius', 2011);
   var mustang = new Car('Ford', 'Mustang', 2013);
-  
+
   //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-  
+
   prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
   mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
